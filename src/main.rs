2a9f042a6ba19cf2576b835874c09a9203fb1a98
Max Curzi <massimiliano.curzi@gmail.com>
2023-04-04 10:13:06 +0100

    cleanup comments

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use text_parser::extract_words;
 
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
